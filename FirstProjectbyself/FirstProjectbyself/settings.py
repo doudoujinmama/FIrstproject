450402c8a76960d1257950948f54692d21190d23
doudoujinmama <18501225267@163.com>
2017-12-28 17:34:36 +0800

    迁移user应用数据库

--- FirstProjectbyself/FirstProjectbyself/settings.py
+++ FirstProjectbyself/FirstProjectbyself/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'FirstProjectbyself.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'github',
+        'HOST':'localhost',
+        'PORT': 3306,
+        'USER':'root',
+        'PASSWORD':'mysql'
     }
 }
 
@@ -88,7 +92,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'zn-Hans'
 
-TIME_ZONE = 'Area/Shanghai'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
