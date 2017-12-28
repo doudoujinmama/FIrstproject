b0e91514b3c5262bb65ba6ef35ffaea20c63992e
doudoujinmama <18501225267@163.com>
2017-12-28 19:11:13 +0800

    修改日志储存路径

--- FirstProjectbyself/FirstProjectbyself/settings.py
+++ FirstProjectbyself/FirstProjectbyself/settings.py
@@ -111,3 +111,89 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, '/static')]
 
 AUTH_USER_MODEL = "user.User"
 
+# 设置日志信息
+
+LOGGING_CONFIG = None
+
+import logging.config
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'filters': {
+        'special': {
+            '()': 'project.logging.SpecialFilter',
+            'foo': 'bar',
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+            'filters': ['special']
+        },
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+
+            # 'level': 'INFO',
+            #
+            # # TimedRotatingFileHandler会将日志按一定时间间隔写入文件中，并
+            #
+            # # 将文件重命名为'原文件名+时间戮'这样的形式
+            #
+            # 'class': 'logging.handlers.TimedRotatingFileHandler',
+
+            'formatter': 'default',
+
+            # 后面这些会以参数的形式传递给TimedRotatingFileHandler的
+            # 构造器
+            # filename所在的目录要确保存在
+
+            # 'filename': 'log',
+            # # 每5分钟刷新一下
+            #
+            # 'when': 'M',
+            #
+            # 'interval': 1,
+            #
+            # 'encoding': 'utf8',
+
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['null'],
+            'propagate': True,
+            'level': 'INFO',
+        },
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'myproject.custom': {
+            'handlers': ['console', 'mail_admins'],
+            'level': 'INFO',
+            'filters': ['special']
+        }
+    }
+}
+logging.config.dictConfig(LOGGING)
